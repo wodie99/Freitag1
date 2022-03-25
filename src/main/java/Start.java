52301fc5ef2f7c28a84663e2a5689abff7125d56
wodie99 <wodie99@wodie.net>
2022-03-25 10:14:32 +0100

    first commit

---
+++ src/main/java/Start.java
@@ -0,0 +1,9 @@
+public class Start {
+    public static void main(String[] args) {
+
+    }
+
+    public static int add (int zahl1, int zahl2) {
+        return zahl1 + zahl2;
+    }
+}
---
+++ src/test/java/StartTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StartTest {
+
+    @Test
+    void add() {
+        assertEquals(5,Start.add(3,2));
+    }
+}
