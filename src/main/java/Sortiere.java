ecc8eb6753abd552a5941d80a3daf02b9fdfc41f
wodie99 <wodie99@wodie.net>
2022-03-27 12:32:28 +0200

    add methode sortR (rekursiv)

--- src/main/java/Sortiere.java
+++ src/main/java/Sortiere.java
@@ -1,22 +1,43 @@
 public class Sortiere {
     public static void main(String[] args) {
-
-        int[] intArray = {8,3,5,1,0,0,17,-1};
+        // itterativer Ansatz
+        int[] intArray = {8, 3, 5, 1, 0, 0, 17, -1};
 
         printArray(intArray);
         sort(intArray);
         printArray(intArray);
+
+        // rekursiver Ansatz
+        //int[] intArray2 = {8, 3, 5, 1, 0, 0, 17, -1};
+        int[] intArray2 = {};
+
+        printArray(intArray2);
+        sortR(intArray2, intArray2.length);
+        printArray(intArray2);
     }
 
 
     public static void sort(int[] numArray) {
-        for (int i = numArray.length; i > 1 ; i--) {
-            for (int j = 0; j < (i-1); j++) {
-                if(numArray[j] > numArray[j + 1]) {
+        for (int i = numArray.length; i > 1; i--) {
+            for (int j = 0; j < (i - 1); j++) {
+                if (numArray[j] > numArray[j + 1]) {
                     // exchange Value of both variabls
-                    swapArrayValue(numArray, j, j+1);
+                    swapArrayValue(numArray, j, j + 1);
                 }
             }
+        }
+    }
+
+    public static void sortR(int[] numArray, int size) {
+        if (size < 2) {
+        } else {
+            for (int i = 0; i  < size - 1; i++) {
+                if (numArray[i] > numArray[i + 1]) {
+                    // exchange Value of both variabls
+                    swapArrayValue(numArray, i, i + 1);
+                }
+            }
+            sortR(numArray, size - 1);
         }
     }
 
@@ -28,7 +49,7 @@ public class Sortiere {
     }
 
     public static void printArray(int[] numArray) {
-            System.out.print("{ ");
+        System.out.print("{ ");
         for (int i = 0; i < numArray.length; i++) {
             System.out.print(numArray[i] + ", ");
         }
